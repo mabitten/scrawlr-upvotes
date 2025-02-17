9fdd34c32a22670d301cb24f2a8fff67ac5d6e12
Matheus Bittencourt <mthbittencourt@gmail.com>
2025-02-16 22:23:30 -0300

    chore: update vite and tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+// vite.config.ts
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setup.ts',
+    css: true,
+  },
 })
