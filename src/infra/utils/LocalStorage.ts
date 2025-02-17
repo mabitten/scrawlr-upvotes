f1bae0d06b40e8217da532f606b54d62954367b3
Matheus Bittencourt <mthbittencourt@gmail.com>
2025-02-16 22:26:11 -0300

    feat: create localStorage infra

---
+++ src/infra/utils/LocalStorage.ts
@@ -0,0 +1,35 @@
+export class LocalStorage {
+  static getItem<T>(key: string, defaultValue: T): T {
+    try {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : defaultValue;
+    } catch (error) {
+      console.error(`Error getting item ${key} from localStorage`, error);
+      return defaultValue;
+    }
+  }
+
+  static setItem<T>(key: string, value: T): void {
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(`Error setting item ${key} to localStorage`, error);
+    }
+  }
+
+  static removeItem(key: string): void {
+    try {
+      localStorage.removeItem(key);
+    } catch (error) {
+      console.error(`Error removing item ${key} from localStorage`, error);
+    }
+  }
+
+  static clear(): void {
+    try {
+      localStorage.clear();
+    } catch (error) {
+      console.error('Error clearing localStorage', error);
+    }
+  }
+} 
