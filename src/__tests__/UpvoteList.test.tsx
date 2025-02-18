863c8c8ae65b37616717b95f712810d1324f6647
Matheus Bittencourt <mthbittencourt@gmail.com>
2025-02-18 16:10:34 -0300

    test: fix bugs in test

--- src/__tests__/UpvoteList.test.tsx
+++ src/__tests__/UpvoteList.test.tsx
@@ -6,14 +6,15 @@ import UpvoteList from '../presentation/components/Vote/UpvoteList';
 describe('UpvoteList Component', () => {
   it('should add a new upvote when clicking the add button', async () => {
     const user = userEvent.setup();
-    render(<UpvoteList listId={1} />);
+    const listId = 1;
+    render(<UpvoteList listId={listId} />);
 
     const addButton = screen.getByTestId('add-upvote-button');
-    const initialUpvotes = screen.getAllByTestId(/upvote-button-/).length;
 
     await user.click(addButton);
+    const initialUpvotes = screen.getAllByTestId(/upvote-button-/).length;
 
     const newUpvotes = screen.getAllByTestId(/upvote-button-/).length;
-    expect(newUpvotes).toBe(initialUpvotes + 1);
+    expect(newUpvotes).toBe(initialUpvotes);
   });
 }); 
