2525fa46f983a328a83d3898a3b19768a516d87a
Matheus Bittencourt <mthbittencourt@gmail.com>
2025-02-18 16:17:12 -0300

    feat: add error boundary for future implementations

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import { UpvoteList, CardFill as UpvoteCard } from "./components";
+import ErrorBoundary from './components/ErrorBoundary';
 
 const App: React.FC = () => {
   return (
+    <ErrorBoundary>
       <div className="container mx-auto p-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <UpvoteCard title="List 1">
@@ -12,6 +15,7 @@ const App: React.FC = () => {
           </UpvoteCard>
         </div>
       </div>
+    </ErrorBoundary>
   );
 };
 
